484513649217ece23fa5d61437d92a3915f141c3
bipinlongjam <41874001+bipinlongjam@users.noreply.github.com>
2022-04-01 19:11:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/App.css';
+import image from './image.jpeg';
 
 const App = () => {
   return (
     <div id="main">
-
+  <img src={image} alt="image" id="resizable-img" height="320px" width="320px"/>
     </div>
   )
 }
